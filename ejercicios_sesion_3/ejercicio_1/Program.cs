b23a80e5652759b0b9865394806cfa772acc34fe
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-02-25 16:39:00 -0300

    ejercicio 1 de la sesion 3 resuelto

---
+++ ejercicios_sesion_3/ejercicio_1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+var pablo = new Cliente("Pablo","+54 123456789","calle falsa 123","pablo@demo.com",false);
+
+Console.WriteLine(pablo);
+
+
+
+public struct Cliente
+{
+    public Cliente(string nombre, string telefono, string direccion, string email, bool es_nuevo)
+    {
+        Nombre = nombre;
+        Telefono = telefono;
+        Direccion = direccion;
+        Email = email;
+        Es_nuevo_cliente = es_nuevo;
+    }
+
+    public string Nombre { get; }
+    public string Telefono { get; }
+    public string Direccion { get; }
+    public string Email { get; }
+    public bool Es_nuevo_cliente { get; }
+
+
+    public override string ToString() => $"Cliente:\n\t Nombre: {Nombre} \n\t Telefono: {Telefono} \n\t Direccion: {Direccion} \n\t Email: {Email} \n\t Es cliente nuevo : {Es_nuevo_cliente}";
+}
+
+
