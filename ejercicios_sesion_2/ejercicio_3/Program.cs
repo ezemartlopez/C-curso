179c41f47c628de21c4619bc6e91dc40c271948c
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-02-24 23:57:21 -0300

    ejercicio 3 listo

---
+++ ejercicios_sesion_2/ejercicio_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Operadores
+int numero = 35;
+bool mayor = numero >= 18;
+Console.WriteLine($"{numero} es mayor que 18: {mayor}");
+
+char caracter = 't';
+bool igual = caracter == 'a';
+Console.WriteLine($"{caracter} es igual al char 'a': {igual}");
+
+bool and = mayor && igual;
+Console.WriteLine($"Condicion: {mayor} && {igual}: {and}");
+
+bool or = mayor || igual;
+Console.WriteLine($"Condicion: {mayor} || {igual}: {or}");
