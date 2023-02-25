d75658940e518c7078753ea34d212bfe192b7d45
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-02-24 23:56:48 -0300

    ejercicio 1 listo

---
+++ ejercicios_sesion_2/ejercicio_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Ejercicio 1
+string? nombre;
+string? apellido;
+int edad;
+string? sabe_programar;
+//Ingreso de las variables
+Console.Write("Ingrese su nombre: ");
+nombre = Console.ReadLine();
+
+Console.Write("Ingrese su apellido: ");
+apellido = Console.ReadLine();
+
+Console.Write("Ingrese su edad: ");
+edad = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ingrese si sabe programar[si/no]: ");
+sabe_programar = Console.ReadLine();
+
+string mensaje = $"> {nombre} {apellido}, tienes {edad} años y {(sabe_programar=="si" ? "sabes programar" :"no sabes programar")}";
+Console.WriteLine(mensaje);
