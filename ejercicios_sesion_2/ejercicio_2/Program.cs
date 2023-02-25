af764f08eb1b9f986facb17c85d1892c289c4729
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-02-24 23:57:08 -0300

    ejercicio 2 listo

---
+++ ejercicios_sesion_2/ejercicio_2/Program.cs
@@ -0,0 +1,11 @@
+﻿//variables que describen a un coche
+int puertas = 4;
+int ruedas = 4;
+string marca = "Audi";
+string ITV_vigente = "B-09";
+
+//variables que describen a una mesa
+double peso = 15.5d;//Kg
+double largo = 2.3d;//Mtrs
+string material = "Roble";
+string color = "Claro";
