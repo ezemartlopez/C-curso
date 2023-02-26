53172d5b824c0ad910c0c8ff1553fe8ab523b4af
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-02-26 10:49:58 -0300

    ejercicio 2 resuelto

---
+++ ejercicios_sesion_4/ejercicio_2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int positivos = 0;
+int negativos = 0;
+int numero;
+do
+{
+    Console.Write("Ingrese un numero entero [(ctrl + c) para salir]: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+    if(numero >= 0){
+        positivos++;
+        Console.WriteLine("Es un numero positivo");
+    }
+    else
+    {
+        negativos++;
+        Console.WriteLine("Es un numero negativo");
+    }
+    Console.WriteLine($"Total de positivos: {positivos} \nTotal de negativos: {negativos}");
+} while (true);
+
