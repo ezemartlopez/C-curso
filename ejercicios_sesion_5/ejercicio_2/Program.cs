508b7440e1ebfbc33024c6e3c4e009962ff68628
Ezequiel Martinez Lopez <zenitram.e14@gmail.com>
2023-02-26 23:16:58 -0300

    ejercicio 2 resuelto

---
+++ ejercicios_sesion_5/ejercicio_2/Program.cs
@@ -0,0 +1,25 @@
+﻿var lenguajes = new List<string> {"C#","Java","C++"};
+int i = 1;
+Console.WriteLine("Elija una opcion: ");
+foreach (var lenguaje in lenguajes)
+{
+    Console.WriteLine($"{i}) {lenguaje}");
+    i++;
+}
+Console.Write("\n opcion: ");
+int opcion = Convert.ToInt32(Console.ReadLine());
+switch (opcion)
+{
+    case 1:
+        Console.WriteLine("Hola, Mundo con C#!!");
+        break;
+    case 2:
+        Console.WriteLine("Hola, Mundo con Java!!");
+        break;
+    case 3:
+        Console.WriteLine("Hola, Mundo con C++ !!");
+        break;
+    default:
+        Console.WriteLine("Esta opcion no existe");
+        break;
+}
